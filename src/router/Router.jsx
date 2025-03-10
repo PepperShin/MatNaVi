160231ec03eaf83fe63eb93764ffd02a5600b501
Fellas <neuneu12@naver.com>
2025-03-10 18:29:31 +0900

    NaverAPI

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,12 +1,19 @@
 import { createBrowserRouter } from 'react-router-dom'
 import TempPage from '../UI/pages/TempPage'
 import MainLayout from '../UI/layouts/MainLayout'
+import NaverMapPage from '../UI/pages/NaverMapPage'
 
 const routes = [
     {
         path: '/',
         element: <MainLayout />
-    }
+    },
+
+    {
+        path: '/NaverMapPage',
+        element: <NaverMapPage />,
+        title: '네이버 맵',
+    },
 ]
 
 const router = createBrowserRouter(routes)
